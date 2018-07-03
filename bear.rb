db345b5b854ba97d79b7202de407493b3f7e4908
Joanna Gora <joanna.gora@protonmail.com>
2018-07-03 21:17:28 +0100

    passed name test

---
+++ bear.rb
@@ -0,0 +1,9 @@
+class Bear
+  attr_reader :name, :type, :stomach
+  def initialize name, type
+    @name = name
+    @type = type
+    @stomach = 0
+  end
+
+end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -3,8 +3,13 @@ require_relative("../bear")
 
 class TestBear < MiniTest::Test
   def setup
-
+    @bear = Bear.new("Monte", "Panda")
   end
 
+  def test_name
+    expected = "Monte"
+    actual = @bear.name
+    assert_equal(expected, actual)
+  end
 
 end
